11ca8202861ff405aee9640ce371f6970d9ceb72
Sérgio Costa <sergio.md.costa@protonmail.com>
2026-01-20 22:25:20 +0000

    feat(config): Load the config of redis env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"github.com/sergioc0sta/limit-barrier/configs"
 )
 
 func main() {
+	v, err := configs.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	println("Config loaded:", v.RedisAddr)
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
